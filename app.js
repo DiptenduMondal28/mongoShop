d9add3179a53f74568b72f501e8d2bfe8c48c6f9
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-08-09 17:51:16 +0530

    Introduction to Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +20,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('64ca5437202b9cd5779f7560')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('64ca5437202b9cd5779f7560')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+const username = process.env.USERNAME_MONGO;
+const password = process.env.PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.vuytclo.mongodb.net/shop?retryWrites=true&w=majority`).then(result=>{
+  console.log('connected!');
   app.listen(3000);
-});
+}).catch(err=>{
+  console.log(err);
+})
